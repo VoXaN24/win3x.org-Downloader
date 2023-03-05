36c4c02c5cec21c078019e62d7946026dacf5071
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2023-03-06 00:11:34 +0100

    Update win3xdl.py

--- win3xdl.py
+++ win3xdl.py
@@ -1,32 +1,42 @@
 import requests
 import os
+import concurrent.futures
 from tqdm import tqdm
 
-def verifier_urls():
-    # Générer les URLs à vérifier pour les IDs de 1 à 10000
+def verifier_url(url):
+    headers = {
+        'Referer': 'http://www.win3x.org',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0'
+    }
+    response = requests.head(url, headers=headers, allow_redirects=True)
+    if response.status_code == 200:
+        return url
+    else:
+        return None
+
+def tester_urls_existantes():
     urls = []
     for i in range(1, 10001):
-        url = f"http://www.win3x.org/win3board/ext/win3x/download.php?id={i}"
+        url = f'http://www.win3x.org/win3board/ext/win3x/download.php?id={i}'
         urls.append(url)
 
-    # Vérifier l'existence de chaque URL avec une barre de progression
-    urls_existantes = []
-    headers = {'Referer': 'http://www.win3x.org', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0'}
-    with tqdm(total=len(urls), desc='Vérification en cours', unit='URL') as pbar:
-        for url in urls:
-            response = requests.head(url, headers=headers)
+    valid_urls = []
 
-            if response.status_code == 200:
-                urls_existantes.append(url)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(verifier_url, url) for url in urls]
 
-            pbar.update(1)
+        for future in tqdm(concurrent.futures.as_completed(futures), total=len(futures), desc="Vérification des URLs"):
+            result = future.result()
+            if result is not None:
+                valid_urls.append(result)
 
-    # Enregistrer les URLs existantes dans un fichier urls_existantes.txt
-    with open('urls_existantes.txt', 'w') as f:
-        f.write('\n'.join(urls_existantes))
+    with open("url.txt", "w") as f:
+        for url in valid_urls:
+            f.write(url + "\n")
 
-    tqdm.write(f"{len(urls_existantes)} URLs existantes ont été enregistrées dans le fichier urls_existantes.txt.")
-    
+if __name__ == '__main__':
+    tester_urls_existantes()
+  
 def dl():
     # Créer le dossier de téléchargement s'il n'existe pas déjà
     if not os.path.exists('dl'):
@@ -51,5 +61,5 @@ def dl():
             tqdm.write(f"{filename} a été téléchargé avec succès dans le dossier dl.")
 
     tqdm.write(f"Tous les fichiers ont été téléchargés avec succès dans le dossier dl.")
-verifier_urls()
+tester_urls_existantes()
 dl()
