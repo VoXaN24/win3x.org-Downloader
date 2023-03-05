2a95079433c7fefa7beb5f73070948bdac6ea396
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2023-03-06 00:26:25 +0100

    Update win3xdl.py

--- win3xdl.py
+++ win3xdl.py
@@ -50,7 +50,8 @@ def dl():
         response = requests.get(url, headers=headers, stream=True)
 
         if response.status_code == 200:
-            with open(os.path.join('dl', filename), 'wb') as f:
+            filename = response.headers.get('Content-Disposition', '').split('filename=')[-1].strip('\"')
+            with open(os.path.join("dl", filename), "wb") as f:
                 for chunk in response.iter_content(chunk_size=8192):
                     if chunk:
                         f.write(chunk)
